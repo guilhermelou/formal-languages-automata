539c48a336e4eefa6111177e575f6e094937a93a
Guilherme Henrique <g.lourenco.santos@gmail.com>
2015-12-02 12:15:22 -0200

    draw transition straight

--- jsflap.js
+++ jsflap.js
@@ -10,14 +10,46 @@ var Transition = function(pattern,next) {
 	this.pattern = pattern;
 	this.next = next;
 	// VISUAL PROPERTIES
-	// property used to determine if the line is straight=0, curve top=1, curve bottom=2
+	// property used to determine if the line is straight=0, curve top=1, curve bottom=-1
     this.bridge = 0;
 };
 
 //BEGIN OF TRANSITION METHODS
 //Method to draw a single transition
-Transition.prototype.drawTransition = function(){
+Transition.prototype.drawTransition = function(origin){
+  	orig_x = origin.x;
+  	orig_y = origin.y;
+  	dest_x = this.next.x;
+  	dest_y = this.next.y;
+  	radius = this.next.radius;
+	color = "black";
+	/*//label
+	_context.fillStyle = "black";
+	var fontsize = 14;
+  	_context.font = fontsize+"px Arial";
+  	var x, y;
+  	x = (dest_x - orig_x)/2 + orig_x;
+  	y = (dest_y - orig_y)/2 + orig_y - 10;
+  	
+  	//	console.log("LENGTH = " + ligacaoAtual.getTransicoes().length);
+  	for (var i = 0; i < ligacaoAtual.getTransicoes().length; i++) 
+  	{
+  		var text = ligacaoAtual.getTransicoes()[i].getTransicao();
+  		var textSize = _context.measureText (text);
+		var width = textSize.width;
+  		_context.fillText(text, x, y);
+  		ligacaoAtual.getTransicoes()[i].setRect(x - 2.5, y - 10, width+5, width+5);
+  		y -= 15;
+  	};*/
+  	
+  	//calculate the arrow
+  	arrow = calculateArrow(orig_x, orig_y, dest_x, dest_y, radius);
+    
+    //line
+    drawLine(orig_x, orig_y, arrow.mid_x, arrow.mid_y, color);
 
+	//arrow
+	drawArrow(arrow,color);
 };
 //END OF TRANSITION METHODS
 
@@ -27,7 +59,7 @@ Transition.prototype.drawTransition = function(){
 var State = function(){
 	//Start without any transitions
 	//w3 recommends avoid new Array()
-	this.transition = [];
+	this.transitions = [];
 	//All booleans will be false by default
 	this.active = false;
 	this.ini = false;
@@ -36,8 +68,8 @@ var State = function(){
 	this.x = 0;
     this.y = 0;
     this.color;
-    this.ray = 20;
-    this.label = label;
+    this.radius = 20;
+    this.label = "";
 };
 
 //BEGIN OF STATE METHODS
@@ -46,7 +78,7 @@ var State = function(){
 //Change codes like state1.transation.push(trans)
 //To codes like state1.addTransation(trans)
 State.prototype.addTransition = function(trans){
-	this.transition.push(trans);
+	this.transitions.push(trans);
 };
 
 // Methods to make easier to change and get x, y properties
@@ -77,7 +109,7 @@ State.prototype.drawTransitions = function(){
 //Don't need parameters
 //Automaton is a list of states
 var Automaton = function(){
-	this.state = [];
+	this.states = [];
 };
 
 //BEGIN OF AUTOMATON METHODS
@@ -127,16 +159,109 @@ Step.prototype.checkStep = function (){
 
 //BEGIN OF FUNCTIONS
 //BEGIN OF CANVAS FUNCTIONS
-//function use to load the var _canvas with the proper element
-function initCanvas()
+
+//function used to clear the canvas
+function clearCanvas()
 {
-	_canvas = document.getElementById("drawCanvas");
-	if (_canvas.getContext){
-      _context = _canvas.getContext('2d');
-    }
+	_context.clearRect(0, 0, _canvas.width, _canvas.height);
 };
+
+//function to draw a line
+function drawLine(x1, y1, x2, y2, color)
+{
+	_context.beginPath();
+	_context.moveTo(x1, y1);
+	_context.lineTo(x2, y2);
+	_context.strokeStyle = color;
+	_context.stroke();
+	_context.closePath();
+};
+
+//calculating arrow
+function calculateArrow(orig_x, orig_y, dest_x, dest_y, radius)
+{
+    var v_link = [];
+    n = [];
+    var nx = 1;
+    var ny;
+    v_link[0] = dest_x - orig_x + 0.01;
+    v_link[1] = dest_y - orig_y + 0.01;
+    var norma = Math.sqrt(Math.pow(v_link[0], 2) + Math.pow(v_link[1], 2));
+    v_link[0] /= norma;
+    v_link[1] /= norma;
+    var pX = dest_x - radius*v_link[0];//arrow in x
+    var pY = dest_y - radius*v_link[1];//arrow in y
+    //get the three points
+    var leftpointX, leftpointY, rightpointX, rightpointY, midpointX, midpointY;
+    midpointX = pX;
+    midpointY = pY;
+ 
+    ny = -(v_link[0])/v_link[1];
+    n[0] = nx;
+    n[1] = ny;
+    norma = Math.sqrt(Math.pow(n[0], 2) + Math.pow(n[1], 2));
+    n[0] /= norma;
+    n[1] /= norma;
+    leftpointX = (pX - (6)*v_link[0] + (6)*n[0]);
+    leftpointY = (pY - (6)*v_link[1] + (6)*n[1]);
+    rightpointX = (pX - (6)*v_link[0] - (6)*n[0]);
+    rightpointY = (pY - (6)*v_link[1] - (6)*n[1]);
+    arrow = {
+    	"left_x": leftpointX,
+    	"left_y": leftpointY,
+    	"right_x": rightpointX,
+    	"right_y": rightpointY,
+    	"mid_x": midpointX,
+    	"mid_y": midpointY};
+    return arrow
+};
+
+//function to draw the arrow 
+function drawArrow(arrow, color)
+{
+	_context.beginPath();
+	_context.moveTo(arrow.mid_x, arrow.mid_y);
+	_context.lineTo(arrow.right_x, arrow.right_y);
+	_context.lineTo(arrow.left_x, arrow.left_y);
+	_context.lineTo(arrow.mid_x, arrow.mid_y);
+	_context.fillStyle = color;
+	_context.fill();
+	_context.closePath();
+};
+
+//function used to draw a transition preview right before the mouse is released
+function drawTransitionPreview(x1, y1, x2, y2)
+{
+	clearCanvas();
+	drawLine(x1, y1, x2, y2, "gray");
+	//drawTransitions();
+	//drawStates();
+};
+
 //function used to clear the canvas and redraw everything
 function updateCanvas()
 {
-	//_canvas = document.getElementById("drawCanvas");
+	clearCanvas();
+};
+
+//function use to load the var _canvas with the proper element
+function initCanvas(canvas_id)
+{
+
+	_canvas = document.getElementById(canvas_id);
+	if (_canvas.getContext){
+      _context = _canvas.getContext('2d');
+    }
+    
+    //TESTANDOOOO
+    drawTransitionPreview(0,0,200,200);
+    drawTransitionPreview(0,0,200,50);
+    state1 = new State();
+    state2 = new State();
+    trans = new Transition("a",state2);
+    state1.addTransition(trans)
+    state1.setXY(10,20);
+    state2.setXY(150,260);
+    trans.drawTransition(state1);
+    //desenhaLigacaoAtual();
 };
